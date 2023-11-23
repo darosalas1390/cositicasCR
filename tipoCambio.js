f020a826241bfe820eea3c12ddac43eb9391eda4
darosalas1390 <134178719+darosalas1390@users.noreply.github.com>
2023-11-22 22:05:29 -0600

    Update tipoCambio.js

--- tipoCambio.js
+++ tipoCambio.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Hacer una solicitud a la API de tipo de cambio
+   
     fetch('https://tipodecambio.paginasweb.cr/api')
         .then(response => response.json())
         .then(data => {
-            // Manejar la respuesta y mostrar el tipo de cambio en el elemento con id 'tipoCambioInfo'
+            
             const tipoCambioInfo = document.getElementById('tipoCambioInfo');
             tipoCambioInfo.innerHTML = `Compra: ${data.compra} | Venta: ${data.venta} | Fecha: ${data.fecha}`;
         })
